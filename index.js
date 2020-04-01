9303b9e8827d4f3da9043b7578b52ba57c7616d7
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-04-01 14:06:51 -0400

    another test

--- index.js
+++ index.js
@@ -82,11 +82,11 @@ app.delete("/todos/:id", (req, res) => {
 });
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 // Anything that doesn't match the above, send back index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
